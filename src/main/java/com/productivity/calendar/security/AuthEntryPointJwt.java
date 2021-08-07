1c4abb8774edf7b6aec9a2ac1e9d60a4bcfe2bc6
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-08-07 15:35:37 -0600

    Apply a correct format to the imports and remove the unused ones.

--- src/main/java/com/productivity/calendar/security/AuthEntryPointJwt.java
+++ src/main/java/com/productivity/calendar/security/AuthEntryPointJwt.java
@@ -1,15 +1,15 @@
 package com.productivity.calendar.security;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
+import org.slf4j.Logger;
+
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
@@ -22,5 +22,4 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         logger.error("Unauthorized error: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
-
 }
