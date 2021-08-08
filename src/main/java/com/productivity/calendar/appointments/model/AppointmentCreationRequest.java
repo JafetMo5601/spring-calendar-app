e6834eac33a9ac484dc7ecf67cace378b456a0da
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-08-07 20:18:01 -0600

    Rename the appointment request to be more descriptive.

--- src/main/java/com/productivity/calendar/appointments/model/AppointmentCreationRequest.java
+++ src/main/java/com/productivity/calendar/appointments/model/AppointmentCreationRequest.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 
-public class CreationRequest {
+public class AppointmentCreationRequest {
 
     @NotBlank
     @NotNull
